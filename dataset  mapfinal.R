00e2ffeeba99e9fb426078f779c5c64f90a53e6d
sic1203 <42786352+sic1203@users.noreply.github.com>
2018-10-18 11:28:32 -0400

    insert single quote again,failed

--- dataset  mapfinal.R
+++ dataset  mapfinal.R
@@ -15,60 +15,51 @@ require(readxl)
 require(stringr)
 require(RColorBrewer)
 
-Ngbh.load <-readOGR('https://opendata.arcgis.com/datasets/dbd133a206cc4a3aa915cb28baa60fd4_0.geojson')
-Ball.load <-readOGR('https://opendata.arcgis.com/datasets/87f3b2bdfa6646319a56e86521480efd_0.geojson')
-income<- read.csv('C:/Users/csvan/Documents/GitHub/SC_Project2/ScGH_project2/ssi.csv',head=TRUE)
+ckanSQL <- function(url) {
+  # Make the Request
+  r <- RETRY("GET", URLencode(url))
+  # Extract Content
+  c <- content(r, "text")
+  # Basic gsub to make NA's consistent with R
+  json <- gsub('NaN', 'NA', c, perl = TRUE)
+  # Create Dataframe
+  data.frame(jsonlite::fromJSON(json)$result$records)
+}
+
+# Unique values for Resource Field
+ckanUniques <- function(id, field) {
+  url <- paste0("https://data.wprdc.org/api/3/action/datastore_search_sql?sql=SELECT%20DISTINCT(%22", field, "%22)%20from%20%22", id, "%22")
+  c(ckanSQL(URLencode(url)))
+}
+
+Ngh <- sort(ckanUniques("7f438bd0-71c7-4997-a5b8-f12894599215", "Neighborhood")$Neighborhood)
+Total <- sort(ckanUniques("7f438bd0-71c7-4997-a5b8-f12894599215","'Estimate..Total....No.Social.Security.income'")$Estimate..Total....No.Social.Security.income)
+Ssi<- sort(ckanUniques("7f438bd0-71c7-4997-a5b8-f12894599215","'Estimate..Total.'")$Estimate..Total.)           
+
 
 #create ui
-ui <- navbarPage("Higher income, more Ballfileds?",
+ui <- navbarPage("Pitssburgh Neighborhood Income",
                  theme = shinytheme("cosmo"),
-                 # tabPanel("Title",
-                 #          mainPanel(
-                 #            imageOutput("ballimg")
-                 #          )),
-                 #create map panel
-                 tabPanel("Ball fileds in Allegheny",
-                          sidebarLayout(
-                            sidebarPanel(
-                              selectInput("NgbhSelect",
-                                          "Neighborhood",
-                                          levels(Ngbh.load$hood),
-                                          selected = c("Allegheny Center","Banksville"),
-                                          selectize = T,
-                                          multiple = T)
-                            ),
-                            mainPanel(
-                              # Using Shiny JS
-                              shinyjs::useShinyjs(),
-                              # Style the background and change the page
-                              tags$style(type = "text/css", ".leaflet {height: calc(100vh - 90px) !important;}
-                                         body {background-color: #e2dede;}"),
-                              # Map Output
-                              leafletOutput("leaflet")
-                              )
-                          )),
-                 tabPanel("Income Distribution",
+                 tabPanel("Plot",
                           sidebarLayout(
                             sidebarPanel(
                               # Different neighborhood select
                               selectInput("NghSelect",
                                           "Choose the Nehborhood:",
-                                          choices = sort(unique(income$Neighborhood)),
-                                          multiple = TRUE,
-                                          selectize = TRUE,
+                                          choices = Ngh,
                                           selected=c("Allentown", "Arlington","Banksville","East Hills", "East Liberty", "Beechview", "Bedford Dwellings", "Beltzhoover", "Bluff")),
                               #Total Income without ssi Select
                               sliderInput("InSelect",
                                           "Estimate Income without SSI:",
-                                          min = min(income$Estimate..Total....No.Social.Security.income, na.rm = T),
-                                          max = max(income$Estimate..Total....No.Social.Security.income, na.rm = T),
+                                          min = min(Total, na.rm = T),
+                                          max = max(Total, na.rm = T),
                                           value = c("1400", "6000"),
                                           step = 1),
                               #total income select
                               sliderInput("SsiSelect",
                                           "Social Security Income:",
-                                          min=min(income$Estimate..Total.,na.rm=T),
-                                          max=max(income$Estimate..Total.,na.rm=T),
+                                          min=min(Ssi,na.rm=T),
+                                          max=max(Ssi,na.rm=T),
                                           value=c("200","6000"),
                                           step=1),
                               #reset button
@@ -93,54 +84,48 @@ ui <- navbarPage("Higher income, more Ballfileds?",
                  )
 # Define server logic
 server <- function(input, output, session = session) {
-  pal <- colorFactor(c("navy", "red"), domain = Ball.load$active)
-  output$leaflet <- renderLeaflet({
-    # Load green infrastructure filtered data
-    #greenInf <- greenInfInputs()
-    Ngbh<-NgbhInputs()
-    leaflet() %>%
-      addProviderTiles("OpenStreetMap.BlackAndWhite", group =" Street",options = providerTileOptions(noWrap = TRUE)) %>%
-      addProviderTiles("Stamen.Toner", group="Toner",options = providerTileOptions(noWrap = TRUE)) %>%
-      setView(-80,40.45,zoom=12) %>%
-      #add polygon layer
-      addPolygons(data = Ngbh) %>%
-      addCircleMarkers(data=Ball.load,radius=6,color=~pal(Ball.load$active),stroke=FALSE,fillOpacity = 0.5)%>%
-      #add layers control
-      addLayersControl(
-        baseGroups = c("Toner", "Street"),
-        options = layersControlOptions(collapsed = FALSE))
+  Ngh.filter <- ifelse(length(input$NghSelect) > 0, paste0("%20AND%20%22Neighborhood%22%20IN%20(%27", paste0(gsub(" " ,"%20", input$NghSelect), collapse = "%27,%27"), "%27)"), "")
+  IcInput <- reactive({
+    # Build API Query with proper encodes
+    url <- paste0("https://data.wprdc.org/api/3/action/datastore_search_sql?sql=SELECT%20*%20FROM%20%227f438bd0-71c7-4997-a5b8-f12894599215da%22%20WHERE%20%22Estimate..Total....No.Social.Security.income%22%20%3E=%20%27", input$InSelect[1], "%27%20AND%20%22Estimate..Total....No.Social.Security.income%22%20%3C=%20%27", input$InSelect[2], "%27", Ngh.filter)
+    print(url)
+    # Load and clean data
+    No.filter <- ckanSQL(url) %>%
+      mutate(Estimate..Total....No.Social.Security.income = as.numeric(Estimate..Total....No.Social.Security.income))
+    return(No.filter)
   })
-  NgbhInputs <- reactive({
-    Ngbh <- Ngbh.load 
-    if (length(input$NgbhSelect) > 0) {
-      Ngbh<- subset(Ngbh, hood %in% input$NgbhSelect)
-    }
-    return(Ngbh)
+  SsiInput <- reactive({
+    # Build API Query with proper encodes
+    url2 <- paste0("https://data.wprdc.org/api/action/datastore_search_sql?sql=SELECT%20*%20FROM%20%227f438bd0-71c7-4997-a5b8-f12894599215da%22%20WHERE%20%22Estimate..Total.%22%20%3E=%20%27", input$SsiSelect[1], "%27%20AND%20%22Estimate..Total.%22%20%3C=%20%27", input$SsiSelect[2], "%27", Ngh.filter)
+    print(url2)
+    # Load and clean data
+    Ssi.filter <- ckanSQL(url2) %>%
+      mutate(Estimate..Total. = as.numeric(Estimate..Total.))
+    return(Ssi.filter)
   })
-  #income without ssi reactive
-  icInput <- reactive({
-    no.filter <- income %>%
-      # Slider income without social security Filter
-      filter(Estimate..Total....No.Social.Security.income >= input$InSelect[1] & Estimate..Total....No.Social.Security.income <= input$InSelect[2])
-    if (length(input$NghSelect) > 0 ) {
-      no.filter <- subset(no.filter, Neighborhood %in% input$NghSelect)
-    }
-    return(no.filter)
-  })
-  #income with ssi reactive
-  ssiInput <- reactive({
-    ssi.filter <- income %>%
-      filter(Estimate..Total. >= input$SsiSelect[1] & Estimate..Total. <= input$SsiSelect[2])
-    if (length(input$NghSelect) > 0 ) {
-      ssi.filter <- subset(ssi.filter, Neighborhood%in% input$NghSelect)
-    }
-    return(ssi.filter)
-  })
-  # output$ballimg <-renderImage()
+  # #income without ssi reactive
+  # icInput <- reactive({
+  #   no.filter <- income %>%
+  #     # Slider income without social security Filter
+  #     filter(Estimate..Total....No.Social.Security.income >= input$InSelect[1] & Estimate..Total....No.Social.Security.income <= input$InSelect[2])
+  #   if (length(input$NghSelect) > 0 ) {
+  #     no.filter <- subset(no.filter, Neighborhood %in% input$NghSelect)
+  #   }
+  #   return(no.filter)
+  # })
+  # #income with ssi reactive
+  # ssiInput <- reactive({
+  #   ssi.filter <- income %>%
+  #     filter(Estimate..Total. >= input$SsiSelect[1] & Estimate..Total. <= input$SsiSelect[2])
+  #   if (length(input$NghSelect) > 0 ) {
+  #     ssi.filter <- subset(ssi.filter, Neighborhood%in% input$NghSelect)
+  #   }
+  #   return(ssi.filter)
+  # })
   #plot1 , use plotly compare two income in each neighborhood
   output$plot1<-renderPlotly({
-    dat1<- icInput()
-    dat2<-ssiInput()
+    dat1<- IcInput()
+    dat2<-SsiInput()
     dat1$Neighborhood<-factor(dat1$Neighborhood)
     dat2$Neighborhood<-factor(dat2$Neighborhood)
     p1<-plot_ly(dat1,x=~Neighborhood,y=~Estimate..Total....No.Social.Security.income,name="With SSI",type="bar")
@@ -149,7 +134,7 @@ server <- function(input, output, session = session) {
     })
   #create plot about the distribution of without SSI
   output$plot2 <- renderPlotly(({
-    dat3<-icInput()
+    dat3<-IcInput()
     ggplotly(
       ggplot(data =dat3, aes(x=Estimate..Total.))+
         geom_density(fill="blue",alpha=.6)+
@@ -161,7 +146,7 @@ server <- function(input, output, session = session) {
   )
   #create plot about the distribton of SSI
   output$plot3 <- renderPlotly(({
-    dat4<-ssiInput()
+    dat4<-SsiInput()
     ggplotly(
       ggplot(data =dat4, aes(x=Estimate..Total....No.Social.Security.income))+
         geom_density(fill="pink",alpha=.6)+
